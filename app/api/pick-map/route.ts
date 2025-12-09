64342afc90faa1ffae9773c73563cb3b1c6ecf11
문영채 <myc1365@naver.com>
2025-12-09 15:22:19 +0900

    add comment

--- app/api/pick-map/route.ts
+++ app/api/pick-map/route.ts
@@ -21,7 +21,7 @@ export async function POST(request: NextRequest) {
     .token(body.cmdToken)
     .body({
       text: text,
-      responseType: "ephemeral",
+      responseType: "inChannel",
       deleteOriginal: "true",
     })
     .send()
--- domain/dooray/types/index.ts
+++ domain/dooray/types/index.ts
@@ -1,3 +1,7 @@
+/**
+ * @description inChannel: 채널에 메시지 전송
+ * @description ephemeral: 사용자에게만 메시지 전송
+ */
 export type DoorayResponseType = "inChannel" | "ephemeral";
 
 export * from "./bot";
