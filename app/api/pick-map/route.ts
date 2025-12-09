164bd1dccc8921413a3791f61f8335010ec62db2
문영채 <myc1365@naver.com>
2025-12-09 16:39:24 +0900

    edit to return

--- app/api/pick-map/route.ts
+++ app/api/pick-map/route.ts
@@ -2,7 +2,7 @@ import { DoorayBotMessageSender } from "@/domain/dooray/sender";
 import { DooraySlashCommandRequest } from "@/domain/dooray/types";
 import { MAPS } from "@/domain/hots/constants/maps";
 import { sampleSize } from "es-toolkit";
-import { NextRequest, NextResponse } from "next/server";
+import { NextRequest } from "next/server";
 
 export async function POST(request: NextRequest) {
   const body: DooraySlashCommandRequest = await request.json();
@@ -17,16 +17,19 @@ export async function POST(request: NextRequest) {
     ...randomMap.map((it) => `- ${it}`),
   ].join("\n");
 
-  const response = await DoorayBotMessageSender.url(body.responseUrl)
-    .token(body.cmdToken)
-    .body({
-      text: text,
-      responseType: "inChannel",
-    })
-    .send()
-    .then((res) => res.text());
+  //   const response = await DoorayBotMessageSender.url(body.responseUrl)
+  //     .token(body.cmdToken)
+  //     .body({
+  //       text: text,
+  //       responseType: "inChannel",
+  //     })
+  //     .send()
+  //     .then((res) => res.text());
 
   console.log(body, text, response);
 
-  return new NextResponse("success");
+  return Response.json({
+    text: text,
+    responseType: "inChannel",
+  });
 }
