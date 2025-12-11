79a0195967cf9c78d449cb907a786636f0af39d5
문영채 <myc1365@naver.com>
2025-12-12 07:37:08 +0900

    prisma 스키마 변경

---
+++ prisma/migrations/20251211223650_init/migration.sql
@@ -0,0 +1,6 @@
+-- AlterTable
+ALTER TABLE "GamePlayerStats" ALTER COLUMN "kills" DROP NOT NULL,
+ALTER COLUMN "deaths" DROP NOT NULL,
+ALTER COLUMN "takedowns" DROP NOT NULL,
+ALTER COLUMN "damageDone" DROP NOT NULL,
+ALTER COLUMN "damageTaken" DROP NOT NULL;
