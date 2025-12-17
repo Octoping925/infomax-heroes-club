6fdac3a9c46deb03e5eaec997f35b95beb56a27c
문영채 <myc1365@naver.com>
2025-12-17 13:36:57 +0900

    승률 계산식을 소수점 2자리까지만 나오게 수정

--- utils/win-rate.ts
+++ utils/win-rate.ts
@@ -1,3 +1,4 @@
 export function calculateWinRate(win: number, lose: number, draw: number) {
-  return (win + draw / 2) / (win + lose + draw);
+  const winRate = (win + draw / 2) / (win + lose + draw);
+  return Math.round(winRate * 10000) / 100;
 }
